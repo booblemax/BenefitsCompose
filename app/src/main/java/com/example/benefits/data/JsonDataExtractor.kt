70c7cf4841582deae8ec629ca5681091d401a591
Maxim Astapenko <booblemax@yandex.by>
2021-07-06 23:55:47 +0300

    refactored to using card in alone list without details

--- app/src/main/java/com/example/benefits/data/JsonDataExtractor.kt
+++ app/src/main/java/com/example/benefits/data/JsonDataExtractor.kt
@@ -1,23 +1,20 @@
 package com.example.benefits.data
 
+import android.content.Context
+import com.example.benefits.domain.models.AddressModel
+import com.example.benefits.domain.models.BenefitModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class JsonDataExtractor {
 
-    suspend fun loadDataFromJson(): List<BenefitDto> =
+    suspend fun loadDataFromJson(context: Context): List<BenefitModel> =
         withContext(Dispatchers.IO) {
-            listOf(
-                BenefitDto("1", "name1", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("2", "name2", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("3", "name3", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("4", "name4", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("5", "name5", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("6", "name6", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("7", "name7", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("8", "name8", "type1", AddressDto("city1", "street1"), "", "","", "icon1"),
-                BenefitDto("9", "name9", "type1", AddressDto("city1", "street1"), "", "", "","icon1"),
-                BenefitDto("10", "name10", "type1", AddressDto("city1", "street1"), "", "", "", "icon1")
-            )
+            val gson = Gson()
+            context.assets.open("db-seed.json").use {
+                gson.fromJson(it.reader(), object : TypeToken<List<BenefitModel>>() {}.type)
+            }
         }
 }
--- app/src/main/java/com/example/benefits/ui/views/BenefitItemView.kt
+++ app/src/main/java/com/example/benefits/ui/views/BenefitItemView.kt
@@ -1,46 +1,93 @@
 package com.example.benefits.ui.views
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.benefits.R
 import com.example.benefits.domain.models.AddressModel
 import com.example.benefits.domain.models.BenefitModel
+// добавить отступы между итемами,
+// доделать на отображение всех списком без детальной информации,
+// поправить итемы на нормальный вид
+// добавить все скидк
+@Composable
+fun BenefitItem(modifier: Modifier, model: BenefitModel, onItemClicked: () -> Unit) {
+    val context = LocalContext.current
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+    ) {
+        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Image(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 12.dp),
+                painter = painterResource(id = R.drawable.ic_test),
+                contentDescription = null
+            )
+            Row(modifier = Modifier.padding(top = 24.dp)) {
+                Column(
+                    modifier = Modifier.weight(1f).padding(end = 12.dp)
+                ) {
+                    Text(
+                        text = model.name,
+                        style = MaterialTheme.typography.h4
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 12.dp),
+                        text = model.address.toString(),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = 12.dp),
+                        text = model.type,
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.End,
+                ) {
+                    Spacer(modifier = Modifier.height(12.dp))
+                    DiscountView(text = model.discount)
+                    Spacer(modifier = Modifier.height(12.dp))
+                    PromoView(text = model.promo, onClick = {
+                        Toast.makeText(context, "promo clicked ${model.promo}", Toast.LENGTH_SHORT)
+                            .show()
+                    })
+                }
+            }
+            Description(model.description)
+        }
+    }
+}
 
 @Composable
-fun BenefitItem(item: BenefitModel) {
-    Card {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_test),
-                contentDescription = "image icon",
-                modifier = Modifier.padding(start = 12.dp, top = 8.dp, bottom = 8.dp)
-            )
-            Text(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(horizontal = 12.dp),
-                text = item.name,
-                style = MaterialTheme.typography.subtitle1
-            )
-            DiscountView(
-                modifier = Modifier.padding(end = 12.dp),
-                text = item.discount
-            )
-        }
+fun Description(description: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        color = Color.LightGray,
+
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
+            text = description
+        )
     }
 }
 
@@ -48,6 +95,15 @@ fun BenefitItem(item: BenefitModel) {
 @Composable
 fun PreviewBenefitItemView() {
     BenefitItem(
-        item = BenefitModel("1", "name1", "type1", AddressModel("city1", "street1"), "10-15%", "", "","icon1")
-    )
+        model = BenefitModel(
+            "1",
+            "name1",
+            "type1",
+            AddressModel("city1", "street1"),
+            "10-15%",
+            "",
+            "",
+            "icon1"
+        )
+    ) {}
 }
